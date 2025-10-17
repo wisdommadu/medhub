c6ec9ca20fa44ad55be8f00e3e06d1db873f7d2b
wisdommadu <wizdommadu@gmail.com>
2025-10-17 11:24:39 +0100

    new

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='md:mx-10'>
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+
+            {/*---------- Left Section -------*/}
+            <div>
+                <img className='mb-0 w-40' src={assets.logo} alt="" />
+                <p className='w-full md:w-2/3 text-gray-600 leading-6'>
+                    MedHub is your gateway to seamless healthcare. Book export GP consultations, urgent care, and personalized wellness programs instantly anytime,
+                    anywhere.
+                </p>
+            </div>
+
+            {/*---------- Center Section -------*/}
+            <div>
+                <p className='text-xl font-medium mb-5'>COMPANY</p>
+                <ul className='flex flex-col gap-2 text-gray-600'>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact us</li>
+                    <li>Privacy policy</li>
+                </ul>
+            </div>
+
+            {/*---------- Right Section -------*/}
+            <div>
+                <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+                <ul className='flex flex-col gap-2 text-gray-600'>
+                    <li>+234-8022-131-472</li>
+                    <li>numericnature@gmail.com</li>
+                </ul>
+            </div>
+
+        </div>
+
+        {/*---------- Copyright Text--------*/}
+        <div>
+            <hr />
+            <p className='py-5 text-sm text-center'>Copyight 2025@Medhub - All Right Reserved.</p>
+        </div>
+       
+    </div>
+  )
+}
+
+export default Footer
---
+++ frontend/src/pages/Appointment.jsx
@@ -0,0 +1,111 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
+import { assets } from '../assets/assets'
+
+const Appointment = () => {
+const {docId} = useParams()
+const {doctors, currencySymbol} = useContext(AppContext)
+
+const [docInfo,setDocInfo] = useState(null)
+const [docSlots,setDocSlots] = useState([])
+const [slotIndex,setSlotIndex] = useState(0)
+const [slotTime,setSLotTime] = useState('')
+
+
+const fetchDocInfo = async () => {
+  const docInfo = doctors.find(doc=> doc._id === docId)
+  setDocInfo(docInfo)
+   
+}
+
+const getAvailableSlots =  async () => {
+  setDocSlots([])
+    // getting current date
+    let today = new Date()
+
+    for(let i=0 ; i < 7; i++){
+      //getting date with index
+      let currentDate = new Date(today)
+      currentDate.setDate(today.getDate()+i)
+
+      // seetings and time of the date with index
+      let endTime = new Date()
+      endTime.setDate(today.getDate()+i)
+      endTime.setHours(21,0,0,0)
+
+      // setting hours
+      if (today.getDate() === currentDate.getDate()) {
+        currentDate.setHours(currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10)
+        currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 : 0)
+      } else {
+        currentDate.setHours(10)
+        currentDate.setMinutes(0)
+      }
+
+      let timeSlots = []
+
+      while(currentDate < endTime) {
+        let formattedTime = currentDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit'})
+
+        // add slot to array
+        timeSlots.push({
+          datetime: new Date(currentDate),
+          time: formattedTime
+        })
+
+        // Increment current time by 30 minutes
+
+        currentDate.setMinutes(currentDate.getMinutes() + 30)
+      }
+
+      setDocSlots(prev => ([...prev,timeSlots]))
+
+    }
+}
+
+  useEffect(()=>{
+    fetchDocInfo()
+  }, [doctors,docId])
+
+  useEffect(()=>{
+    getAvailableSlots()
+  }, [docInfo])
+
+  useEffect(()=>{
+    console.log(docSlots)
+  }, [docSlots])
+
+  return docInfo && (
+    <div>
+        {/*---------------- Doctors Details ------------*/}
+        <div className='flex flex-col sm:flex-row gap-4'>
+          <div>
+          <img className='bg-primary w-full sm:max-w-72 rounded-lg' src={docInfo.image} alt="" />
+        </div>
+        <div className='flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0'>
+          {/*---------------- Doc Info : name, degree, experince ------------*/}
+          <p className='flex items-center gap-2 text-2xl font-medium text-gray-900'>
+            {docInfo.name} <img className='w-5' src={assets.verified_icon} alt="" />
+          </p>
+          <div className='flex items-center gap-2 text-sm mt-1 text-gray-600'>
+            <p>{docInfo.degree} - {docInfo.speciality}</p>
+            <button className='py-0.5 px-2 border text-xs rounded-full'>{docInfo.experience}</button>
+          </div>
+          {/*---------------- Doctor About ------------*/}
+          <div>
+            <p className='flex items.center gap-1 text-sm font-medium text-gray-900 mt-3'>
+              About <img src={assets.info_icon} alt="" />
+            </p>
+            <p className='text-sm text-gray-500 max-w-[700px] mt-1'>{docInfo.about}</p>
+          </div>
+          <p className='text-gray-500 font-medium mt-4'>
+            Appointment fee: <span className='text-gray-600'>{currencySymbol}{docInfo.fees}</span>
+          </p>
+        </div>
+    </div>
+  </div>
+  )
+}
+
+export default Appointment
