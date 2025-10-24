4461ebcb05ea1f98bd0b2bfff178702cd83a6870
wisdommadu <wizdommadu@gmail.com>
2025-10-24 13:15:36 +0100

    dev1

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
+import connectcloudinary from './config/cloudinary.js'
 
 
 // app config
 const app = express()
 const port = process.env.PORT || 4000
 connectDB()
+connectcloudinary()
 
 // middlewares
 app.use(express.json())
